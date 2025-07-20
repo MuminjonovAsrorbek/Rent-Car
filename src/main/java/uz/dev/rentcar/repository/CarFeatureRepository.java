a386e948aae17b0749dd5bdd1c870a1e495dcfb2
Asrorbek <muminjonovasrorbek2004@gmail.com>
2025-07-20 13:28:29 +0500

    CarFeatureRepository fixed and optimization

--- src/main/java/uz/dev/rentcar/repository/CarFeatureRepository.java
+++ src/main/java/uz/dev/rentcar/repository/CarFeatureRepository.java
@@ -3,12 +3,13 @@ package uz.dev.rentcar.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.http.HttpStatus;
 import uz.dev.rentcar.entity.CarFeature;
+import uz.dev.rentcar.exceptions.EntityNotFoundException;
 
 public interface CarFeatureRepository extends JpaRepository<CarFeature, Long> {
 
     default CarFeature getByIdOrThrow(Long id) {
         return findById(id)
-                .orElseThrow(() -> new CarFeatureNotFoundException(HttpStatus.NOT_FOUND,id));
+                .orElseThrow(() -> new EntityNotFoundException("CarFeature not found with ID : " + id, HttpStatus.NOT_FOUND));
     }
 
 }
