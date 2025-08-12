b4daa81997627dd15716c1ad8dc5c11723269306
Asrorbek <muminjonovasrorbek2004@gmail.com>
2025-08-12 14:46:51 +0500

    Remove Eureka client

--- src/main/java/uz/dev/rentcar/entity/Booking.java
+++ src/main/java/uz/dev/rentcar/entity/Booking.java
@@ -68,7 +68,7 @@ public class Booking extends AbsDeleteEntity {
     @Enumerated(EnumType.STRING)
     private BookingStatusEnum status;
 
-    @OneToOne(mappedBy = "booking", cascade = CascadeType.REMOVE)
+    @OneToOne(mappedBy = "booking", cascade = CascadeType.ALL)
     private Payment payment;
 
     @OneToMany(mappedBy = "booking", cascade = CascadeType.REMOVE, fetch = FetchType.EAGER)
